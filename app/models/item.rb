9e7956936020763e097fd4dcf7adf41ebfd0fc8d
kei-sasaki-rgb-cell <11eftx2tree@gmail.com>
2021-03-02 19:30:02 +0900

    商品出品機能のテストコードを記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,28 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  #空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+
+    validates :category_id
+    validates :state_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :shipping_date_id
+
+    validates :price
+  end
+
+  #選択が「--」の時は保存できないようにする
+  validates :category_id, :state_id, :postage_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品情報' do
+    context '商品が出品できる場合' do
+      it '必須項目に全て記入、選択していれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が出品できない場合' do
+      it '商品名が記入されてなければ保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明がなければ保存できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーを選択していなければ保存できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+      it '商品の状態を選択していなければ保存できない'do
+        @item.state_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank", "State is not a number")
+      end
+      it '配送料の負担を選択していなければ保存できない' do
+        @item.postage_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank", "Postage is not a number")
+      end
+      it '発送元の地域の選択をしていなければ保存できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+      end
+      it '発送までの日数を選択していなければ保存できない' do
+        @item.shipping_date_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank", "Shipping date is not a number")
+      end
+      it '販売価格を記述していなければ保存できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+  end
 end
