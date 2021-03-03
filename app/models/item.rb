8a7dc08a3de47a815a7ab90c663a52ec7c4585a5
kei-sasaki-rgb-cell <11eftx2tree@gmail.com>
2021-03-03 22:41:49 +0900

    販売金額の範囲内の数値のみ保存するようバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :shipping_date_id
 
-    validates :price
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 
   #選択が「--」の時は保存できないようにする
