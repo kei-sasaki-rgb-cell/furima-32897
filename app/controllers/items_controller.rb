082420529b2e592fd4c94dbd3932f43155000973
kei-sasaki-rgb-cell <11eftx2tree@gmail.com>
2021-03-10 19:30:14 +0900

    メンターからの指摘箇所を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_user, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,11 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user == @item.user
   end
 
   def update
-    @item.update(item_params)
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -44,5 +43,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_user
+    redirect_to root_path unless current_user == @item.user
   end
 end
